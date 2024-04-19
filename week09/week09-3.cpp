d4f22a772b4be553cd3aec237efc62b9cf5844cc
hsuchun7 <dorahsu1117@gmail.com>
2024-04-19 10:51:55 +0800

    week09

--- week09/week09-3.cpp
+++ week09/week09-3.cpp
@@ -1,1 +1,17 @@
+///week09-3.cpp
+#include <stdio.h>
+int globalA = 300;
 
+void myFuncA(){
+    int localA = 500;
+    globalA = 0;
+    printf("myFunc(): gloablA:%d\n localA%d\n", globalA, localA);
+}
+
+int main()
+{
+    int localA = 200;
+    printf("main(): gloablA:%d\n localA%d\n", globalA, localA);
+    myFuncA();
+    printf("main(): gloablA:%d\n localA%d\n", globalA, localA);
+}
