648fcec9825a49d919e839e145ae91134c472868
hsuchun7 <dorahsu1117@gmail.com>
2024-06-14 11:46:55 +0800

    week17

--- week17/week17-4.cpp
+++ week17/week17-4.cpp
@@ -1,1 +1,13 @@
-
+// week17-4.cpp
+#include <stdio.h>
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	for(int i=0; i<n; i++){
+		int now;
+		scanf("%d", &now);
+		printf("%d,", now*now);
+	}
+	printf("\n");
+}
