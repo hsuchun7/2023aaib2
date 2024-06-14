071d4605d786a14e3a8a656ce1c2b3fa1be5c5c8
hsuchun7 <dorahsu1117@gmail.com>
2024-06-14 11:08:00 +0800

    week17

---
+++ week02/week02-2.cpp
@@ -0,0 +1,14 @@
+// week02-2.cpp
+#include <stdio.h>
+int main()
+{
+    float pi = 3.14159262;
+    double pi2 = 3.1415926;
+    printf("float %f\n", pi);
+    printf("double %f\n", pi2);
+    printf("float 10位: %10f\n", pi);
+    printf("double 10位: %10f\n", pi2);
+    printf("float 10位: %20.18f\n", pi);
+    printf("double 10位: %20.18f\n", pi2);
+
+}
